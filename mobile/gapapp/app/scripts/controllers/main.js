d38a90c167f3d09f74689e28547bb58a3d49a3a8
Ari Lerner <arilerner@mac.com>
2013-11-12 01:14:57 -0800

    Clean up

--- mobile/gapapp/app/scripts/controllers/main.js
+++ mobile/gapapp/app/scripts/controllers/main.js
@@ -3,7 +3,6 @@
 angular.module('gapappApp')
   .controller('MainCtrl', function($scope, Cordova) {
     Cordova.navigator().then(function(n) {
-      console.log("NAVIGATOR", n);
       navigator.notification.vibrate();
     });
   });
