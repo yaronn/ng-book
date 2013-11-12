a5815d279722376bd3b1515427bb8c7e0fd3bbb4
Ari Lerner <arilerner@mac.com>
2013-11-11 21:10:18 -0800

    Pause

---
+++ mobile/gapapp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('gapappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
